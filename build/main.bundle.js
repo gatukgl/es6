b1972ce9f2a7f8d6b67b6c7367c1bd8873bb284b
Gatuk S Chattanon <gatukgl@gmail.com>
2017-09-01 20:47:36 +0700

    fetch insecure api

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var func = function func() {
-  var response = fetch('https://api.openaura.com/v1/search/artists?q=katy+perry&api_key=1d9ea39e70e3476140c180593a2ae5bd44b65f12').then(function (response) {
+  var response = fetch('http://api.openaura.com/v1/search/artists?q=katy+perry&api_key=1d9ea39e70e3476140c180593a2ae5bd44b65f12').then(function (response) {
     return response.json();
   }).then(function (response) {
     return console.log('response', response);
